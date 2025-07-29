c56cc84a76cf2c813be00abcfca63429c6b99279
serg10arg <cabrerasergio094@gmail.com>
2025-07-29 20:34:01 -0300

    Initial commit

---
+++ src/test/java/com/example/finanzaspersonales/FinanzasPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finanzaspersonales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasPersonalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
